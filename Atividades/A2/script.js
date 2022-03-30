85a2b5e6434bec39428235d0971d819099877c6b
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-03-30 19:03:28 -0300

    add function close modal

--- Atividades/A2/script.js
+++ Atividades/A2/script.js
@@ -1,7 +1,24 @@
+/* VARIÁVEIS MODAL *********************************************/
+
+// botão que ativa modal
 var novo = document.getElementById("novo");
+
+// o modal
 var modal = document.getElementById("modal");
 
-novo.addEventListener("click", Modal);
-function Modal () {
+// botão que faz modal sumir
+var fecha = document.getElementById("reset");
+
+/* FUNÇÕES MODAL **********************************************/
+
+// Função que ativa modal
+novo.addEventListener("click", iniModal);
+function iniModal () {
   modal.classList.add("active");
+}
+
+// Função que desativa modal
+fecha.addEventListener("click", fimModal);
+function fimModal () {
+  modal.classList.remove("active");
 }
