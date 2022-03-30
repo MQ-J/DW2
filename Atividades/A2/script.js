acb3889f7e1f233d5638152f732f8ade97938585
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-03-29 23:55:55 -0300

    JS function show modal

--- Atividades/A2/script.js
+++ Atividades/A2/script.js
@@ -1,1 +1,7 @@
+var novo = document.getElementById("novo");
+var modal = document.getElementById("modal");
 
+novo.addEventListener("click", Modal);
+function Modal () {
+  modal.classList.add("active");
+}
