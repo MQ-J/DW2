4a19da44769e68cbf56e5d609bc55dfa845e9886
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-03-31 08:49:03 -0300

    local storage set

--- Atividades/A2/script.js
+++ Atividades/A2/script.js
@@ -13,12 +13,31 @@ var fecha = document.getElementById("reset");
 
 // Função que ativa modal
 novo.addEventListener("click", iniModal);
+
 function iniModal () {
   modal.classList.add("active");
 }
 
 // Função que desativa modal
 fecha.addEventListener("click", fimModal);
+
 function fimModal () {
   modal.classList.remove("active");
+}
+
+/*VARIÁVEIS STORAGE ************/
+var linha = [];
+var colunas = [];
+var form = document.getElementById("newtransaction");
+
+/*FUNÇÕES STORAGE ***************/
+form.addEventListener("submit", setTabela);
+
+function setTabela() {
+  linha.push(document.getElementById("description").value);
+  linha.push(document.getElementById("valor").value);
+  linha.push(document.getElementById("data").value);
+  colunas.push(linha);
+  localStorage.setItem("tabela", JSON.stringify(colunas));
+  alert(localStorage.getItem("tabela"));
 }
