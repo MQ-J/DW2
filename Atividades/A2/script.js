5313744303166650ba442d65c02a52958f80d6db
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-04-03 18:47:05 -0300

    new JS model

--- Atividades/A2/script.js
+++ Atividades/A2/script.js
@@ -1,43 +1,18 @@
-/* VARIÁVEIS MODAL *********************************************/
+/* MODAL *************/
 
-// botão que ativa modal
-var novo = document.getElementById("novo");
-
-// o modal
+// variável
 var modal = document.getElementById("modal");
 
-// botão que faz modal sumir
-var fecha = document.getElementById("reset");
-
-/* FUNÇÕES MODAL **********************************************/
-
-// Função que ativa modal
-novo.addEventListener("click", iniModal);
-
-function iniModal () {
-  modal.classList.add("active");
+// função
+const Modal = {
+  open() {
+    /*add active class in modal*/
+    modal.classList.add("active");
+  },
+  close() {
+    /*remove active class in modal*/
+    modal.classList.remove("active");
+  }
 }
 
-// Função que desativa modal
-fecha.addEventListener("click", fimModal);
-
-function fimModal () {
-  modal.classList.remove("active");
-}
-
-/*VARIÁVEIS STORAGE ************/
-var linha = [];
-var colunas = [];
-var form = document.getElementById("newtransaction");
-
-/*FUNÇÕES STORAGE ***************/
-form.addEventListener("submit", setTabela);
-
-function setTabela() {
-  linha.push(document.getElementById("description").value);
-  linha.push(document.getElementById("valor").value);
-  linha.push(document.getElementById("data").value);
-  colunas.push(linha);
-  localStorage.setItem("tabela", JSON.stringify(colunas));
-  alert(localStorage.getItem("tabela"));
-}
+/*STORAGE ************/
