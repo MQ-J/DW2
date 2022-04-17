6200d5e62064a25ada7cba727b2ff17bc39f2642
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-04-16 21:46:54 -0300

    demais campos

--- Atividades/A5/src/js/main.js
+++ Atividades/A5/src/js/main.js
@@ -1,6 +1,15 @@
 const masks = {
   cpf (value) {
-    return value.replace(/\D/g,'').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})/, ('$1-$2')).replace(/(-\d{2})\d+?$/, '$1')
+    return value.replace(/\D/g,'').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})/, '$1-$2').replace(/(-\d{2})\d+?$/, '$1')
+  },
+  date (value) {
+    return value.replace(/\D/g,'').replace(/(\d{2})(\d)/, '$1/$2').replace(/(\d{2})(\d)/, '$1/$2').replace(/(\d{4})\d+?$/, '$1')
+  },
+  fone (value) {
+    return value.replace(/\D/g, '').replace(/(\d{2})(\d)/, '($1) $2').replace(/(\d{5})(\d)/, '$1-$2').replace(/(-\d{4})(\d)/, '$1')
+  },
+  cep (value) {
+    return value.replace(/\D/g, '').replace(/(\d{5})(\d)/, '$1-$2')
   }
 }
 
