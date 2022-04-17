6c3b1374ba7b8f17628d394a60f327db5bb8e697
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-04-16 21:51:38 -0300

    corrige cep

--- Atividades/A5/src/js/main.js
+++ Atividades/A5/src/js/main.js
@@ -6,10 +6,10 @@ const masks = {
     return value.replace(/\D/g,'').replace(/(\d{2})(\d)/, '$1/$2').replace(/(\d{2})(\d)/, '$1/$2').replace(/(\d{4})\d+?$/, '$1')
   },
   fone (value) {
-    return value.replace(/\D/g, '').replace(/(\d{2})(\d)/, '($1) $2').replace(/(\d{5})(\d)/, '$1-$2').replace(/(-\d{4})(\d)/, '$1')
+    return value.replace(/\D/g, '').replace(/(\d{2})(\d)/, '($1) $2').replace(/(\d{5})(\d)/, '$1-$2').replace(/(-\d{4})(\d+?$)/, '$1')
   },
   cep (value) {
-    return value.replace(/\D/g, '').replace(/(\d{5})(\d)/, '$1-$2')
+    return value.replace(/\D/g, '').replace(/(\d{5})(\d)/, '$1-$2').replace(/(-\d{3})(\d+?$)/, '$1')
   }
 }
 
