8248b15fccb573b7dc3a9e0f3d5b5c712f0995ab
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-04-29 22:28:16 -0300

    CPF FOI!

--- Atividades/A5/src/js/modules/cpf.js
+++ Atividades/A5/src/js/modules/cpf.js
@@ -9,19 +9,25 @@ export function validaCPF (strCPF) {
     var Soma;
     var Resto;
     Soma = 0;
-    if (strCPF == "00000000000") return false;
-    
-    alert(parseInt(strCPF).substring(1-1, 1))
-    for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+    if (strCPF == "00000000000") {
+      return false
+    }
+
+    for (var i=1; i<=9; i++) {
+      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i)
+    }
     Resto = (Soma * 10) % 11;
+
   
     if ((Resto == 10) || (Resto == 11))  Resto = 0;
     if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
   
     Soma = 0;
-    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    for (var i=1; i<=10; i++) {
+      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+    }
     Resto = (Soma * 10) % 11;
-  
+    
     if ((Resto == 10) || (Resto == 11))  Resto = 0;
     if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
     return true;
