6650f110a3f828bb7016ce3f146ce15eac7aa76f
MQ-J <mqjunioretecgv@gmail.com>
2022-05-25 10:40:13 -0300

    adjust modules design

---
+++ Atividades/A8/dist/main.483464cafba250f8a1ab.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatea7"]("main",{
+
+/***/ "./src/components/ListaCli.jsx":
+/*!*************************************!*\
+  !*** ./src/components/ListaCli.jsx ***!
+  \*************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\src\\components\\ListaCli.jsx: Unexpected token (28:37)\n\n\u001b[0m \u001b[90m 26 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 27 |\u001b[39m     useEffect(() \u001b[33m=>\u001b[39m {\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 28 |\u001b[39m         api\u001b[33m.\u001b[39m\u001b[36mget\u001b[39m(\u001b[32m`/pessoas/shownome/${?nome}/${sobrenome}`\u001b[39m)\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m                                      \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 29 |\u001b[39m             \u001b[33m.\u001b[39mthen((response) \u001b[33m=>\u001b[39m console\u001b[33m.\u001b[39mlog(response\u001b[33m.\u001b[39mdata\u001b[33m.\u001b[39mdata))\u001b[0m\n\u001b[0m \u001b[90m 30 |\u001b[39m             \u001b[33m.\u001b[39m\u001b[36mcatch\u001b[39m((err) \u001b[33m=>\u001b[39m {\u001b[0m\n\u001b[0m \u001b[90m 31 |\u001b[39m                 console\u001b[33m.\u001b[39merror(\u001b[32m\"ops! ocorreu um erro\"\u001b[39m \u001b[33m+\u001b[39m err)\u001b[33m;\u001b[39m\u001b[0m\n    at instantiate (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:72:32)\n    at constructor (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:358:12)\n    at Object.raise (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:3335:19)\n    at Object.unexpected (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:3373:16)\n    at Object.parseExprAtom (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:13015:22)\n    at Object.parseExprAtom (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:8034:20)\n    at Object.parseExprSubscripts (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:12540:23)\n    at Object.parseUpdate (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:12519:21)\n    at Object.parseMaybeUnary (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:12490:23)\n    at Object.parseMaybeUnaryOrPrivate (C:\\Users\\marcos.junior\\DW2\\Atividades\\A8\\node_modules\\@babel\\parser\\lib\\index.js:12284:61)");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("188a0e715d8dc5f30316")
+/******/ })();
+/******/ 
+/******/ }
+);
