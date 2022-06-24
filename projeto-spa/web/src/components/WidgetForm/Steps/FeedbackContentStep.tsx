845e60cc7d3832b0500e3d7c90b07796878071e1
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-06-24 22:51:49 +0000

    next: conteúdo do feedback

---
+++ projeto-spa/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,38 @@
+import { ArrowArcLeft, ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from ".."
+import { CloseButton } from "../../CloseButton"
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: FeedbackContentStepProps) {
+
+    const FeedbackTypeInfo = feedbackTypes[feedbackType]
+
+    return (
+        <>
+            <header>
+                <button 
+                    type="button" 
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                    onClick={onFeedbackRestartRequested}
+                >
+                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                </button>
+
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img src={FeedbackTypeInfo.image.source} alt={FeedbackTypeInfo.image.alt} className="w-6 h-6" />
+                    {FeedbackTypeInfo.title}
+                </span>
+
+                <CloseButton />
+            </header>
+
+            <div className='flex py-8 gap-2 w-full'>
+
+            </div>
+        </>
+    )
+}
