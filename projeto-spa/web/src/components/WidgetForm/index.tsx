188c20300c7d6ff7aefd1809f7fc1c9422b3701c
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-06-26 16:57:04 +0000

    TERMINEI AULA 2

--- projeto-spa/web/src/components/WidgetForm/index.tsx
+++ projeto-spa/web/src/components/WidgetForm/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { CloseButton } from '../CloseButton'
 import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+import { FeedbackSucessStep } from './Steps/FeedbackSucessStep'
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
 
 export const feedbackTypes = {
@@ -32,26 +33,35 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm() {
 
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSend, setFeedbackSend] = useState(false)
 
   function handleRestartFeedback() {
+    setFeedbackSend(false)
     setFeedbackType(null)
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      { feedbackSend ? (
+      <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback}/>
       ) : (
-        <FeedbackContentStep 
-          feedbackType={feedbackType}
-          onFeedbackRestartRequested={handleRestartFeedback}
-        />
+      <>
+        {!feedbackType ? (
+          <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+        ) : (
+          <FeedbackContentStep
+            feedbackType={feedbackType}
+            onFeedbackRestartRequested={handleRestartFeedback}
+            OnFeedbackSend={() => setFeedbackSend(true)}
+          />
+        )}
+      </>
       )}
-      
+
 
       <footer className="text-xl text-neutral-400">
-        Feito com ❤️ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+        Feito com ❤️ pelo <a className="underline underline-offset-2" href="https://github.com/MQ-J">Querino</a>
       </footer>
     </div>
   )
