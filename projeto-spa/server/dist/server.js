476b6e8f23481e0192a661361d0838b02797fda7
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-06-26 21:06:27 +0000

    and now?

--- projeto-spa/server/dist/server.js
+++ projeto-spa/server/dist/server.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.get('/users', (req, res) => {
+    return res.send('hello word');
+});
 app.listen(3333, () => {
     console.log('HTTP SERVER RUNING !');
 });
--- projeto-spa/server/src/server.ts
+++ projeto-spa/server/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express();
 
+app.get('/users', (req, res) => {
+    return res.send('hello word')
+})
+
 app.listen(3333, () => {
     console.log('HTTP SERVER RUNING !')
 })
