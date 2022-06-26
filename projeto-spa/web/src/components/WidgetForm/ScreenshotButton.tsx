e97655ce68b0fc7b0e0a54302285244d7e2931aa
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-06-26 16:00:17 +0000

    next: envio do feedback

--- projeto-spa/web/src/components/WidgetForm/ScreenshotButton.tsx
+++ projeto-spa/web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,9 +1,14 @@
 import html2canvas from "html2canvas";
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
 import { Loading } from "../Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+    screenshot: string | null
+    onScreenshotTook: (screenshot: string | null) => void
+}
+
+export function ScreenshotButton({onScreenshotTook, screenshot}: ScreenshotButtonProps) {
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
 
     async function hendleTakeScreenshot() {
@@ -12,9 +17,26 @@ export function ScreenshotButton() {
         const canvas = await html2canvas(document.querySelector('html')!)
         const base64image = canvas.toDataURL('image/png')
         
-        console.log(base64image)
+        onScreenshotTook(base64image)
 
         setIsTakingScreenshot(false)
+    }
+
+    if(screenshot) {
+        return(
+            <button
+                type="button"
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    backgroundPosition: 'right bottom', // TIRAR ISSO QUANDO COLOCAR O WIDGET NUM SITE COMPLETO
+                    backgroundSize: 180, // TIRAR ISSO QUANDO COLOCAR O WIDGET NUM SITE COMPLETO
+                }}
+                onClick={() => onScreenshotTook(null)}
+            >
+                <Trash weight="fill"/>
+            </button>
+        )
     }
 
     return (
